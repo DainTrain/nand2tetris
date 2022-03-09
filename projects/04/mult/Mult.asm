6da990bc147186a63c0f6bfa92d40c84d67c2506
Dain LaRock <daintrain@protonmail.com>
2022-03-08 22:47:05 -0600

    Proj 04 Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,42 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// Continually add R1 to R2, decrementing R0 until R0 equals 0
+// Ends with R1 being added R0 times, which is R1*R0
+// R2 = 0
+@0
+D=A
+@2
+M=D
+
+(LOOP)
+
+// if R0 == 0 then jump to END
+@0
+D=M
+@END
+D;JEQ
+
+// R2 = R2 + R1
+@2
+D=M
+@1
+D=D+M
+@2
+M=D
+
+// R0 = R0 - 1
+@0
+D=M
+@1
+D=D-A
+@0
+M=D
+
+// loop
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
